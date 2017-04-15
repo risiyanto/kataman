ac3e0b25f588acf39b86b854f7fe5fd96d63b3aa
risiyanto <risiyanto@bekas.com>
2017-04-15 11:55:24 +0700

    Fix button copy to clipboard (Salin)

--- app/controller.php
+++ app/controller.php
@@ -3,9 +3,7 @@
 //! Base controller
 class Controller 
 {
-
-	protected
-		$db;
+	protected $db;
 
 	//! HTTP route pre-processor
 	function beforeroute($f3) 
@@ -27,13 +25,6 @@ class Controller
 		// Connect to the database
 		$db=new DB\SQL($f3->get('DB'));
 
-		if (file_exists('setup.sql')) {
-			// Initialize database with default setup
-			$db->exec(explode(';',$f3->read('setup.sql')));
-			// Make default setup inaccessible
-			rename('setup.sql','setop.$ql');
-		}
-		
 		// Save frequently used variables
 		$this->db=$db;
 	}
